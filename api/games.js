d2d807046d6dd61a6e047aa1c474b04a8134e427
Matheus de Menezes <matheus.de.mnzes@gmail.com>
2026-08-12 18:58:54 -0300

    Mudança nos games que aparecem

--- api/games.js
+++ api/games.js
@@ -102,7 +102,8 @@ export default async function handler(request, response) {
                         platforms.name;
 
                     ${pesquisa ? `search "${pesquisa}";` : ""}
-                    where cover != null & rating != null${genero ? ` & genres = (${genero})` : ""};
+                    where cover != null & rating != null & rating_count >= 500 ${genero ? ` & genres = (${genero})` : ""};
+
                     ${pesquisa ? "" : "sort rating desc;"}
 
                     limit ${limite};
