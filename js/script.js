66a856c9fca1da3fb8e206b9f6b9bd5be74f9f54
gabrielg. <gabrielgarciaribeiro18@gmail.com>
2026-08-17 11:30:23 -0300

    Remoção do teste do login
    Remoção do "joao / 1234" e "maria / senha"

--- js/script.js
+++ js/script.js
@@ -8,10 +8,6 @@ let temporizadorPesquisa;
 let catalogoCarregado = false;
 
 const GENEROS = { "Adventure": "Aventura", "Arcade": "Arcade", "Card & Board Game": "Cartas e tabuleiro", "Fighting": "Luta", "Indie": "Independente", "Music": "Música", "Platform": "Plataforma", "Puzzle": "Quebra-cabeça", "Racing": "Corrida", "Role-playing (RPG)": "RPG", "Shooter": "Tiro", "Simulator": "Simulação", "Sport": "Esporte", "Strategy": "Estratégia", "Tactical": "Tático", "Visual Novel": "Visual novel" };
-const USUARIOS_DEMO = [
-    { usuario: "joao", senha: "1234" },
-    { usuario: "maria", senha: "senha" }
-];
 let modoCadastro = false;
 let planoPendente = null;
 
